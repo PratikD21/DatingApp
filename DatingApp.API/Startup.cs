75338df990e30deb460b7630cc75d77b0170fa15
Pratik Darak <darak.pratik@gmail.com>
2018-10-19 13:39:30 +0530

    End of section 17

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace DatingApp.API {
     public class Startup {
@@ -24,15 +25,17 @@ namespace DatingApp.API {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
-            services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
+            services.AddDbContext<DataContext> (x => x.
+                UseMySql (Configuration.GetConnectionString ("DefaultConnection"))
+                    .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning)));
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1)
-            .AddJsonOptions(opt => {opt.SerializerSettings.ReferenceLoopHandling= Newtonsoft.Json.ReferenceLoopHandling.Ignore;});
+                .AddJsonOptions (opt => { opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore; });
             services.AddCors ();
-            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
-            services.AddAutoMapper();
-            services.AddTransient<Seed>();
+            services.Configure<CloudinarySettings> (Configuration.GetSection ("CloudinarySettings"));
+            services.AddAutoMapper ();
+            services.AddTransient<Seed> ();
             services.AddScoped<IAuthRepository, AuthRepository> ();
-            services.AddScoped<IDatingRepository, DatingRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository> ();
             services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme).
             AddJwtBearer (options => {
                 options.TokenValidationParameters = new TokenValidationParameters {
@@ -43,9 +46,30 @@ namespace DatingApp.API {
                 ValidateAudience = false
                 };
             });
-            services.AddScoped<LogUserActivity>();
+            services.AddScoped<LogUserActivity> ();
         }
-
+        public void ConfigureDevelopmentServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1)
+                .AddJsonOptions (opt => { opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore; });
+            services.AddCors ();
+            services.Configure<CloudinarySettings> (Configuration.GetSection ("CloudinarySettings"));
+            services.AddAutoMapper ();
+            services.AddTransient<Seed> ();
+            services.AddScoped<IAuthRepository, AuthRepository> ();
+            services.AddScoped<IDatingRepository, DatingRepository> ();
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme).
+            AddJwtBearer (options => {
+                options.TokenValidationParameters = new TokenValidationParameters {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey (Encoding.ASCII
+                .GetBytes (Configuration.GetSection ("AppSettings:Token").Value)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+                };
+            });
+            services.AddScoped<LogUserActivity> ();
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IHostingEnvironment env, Seed seeder) {
             if (env.IsDevelopment ()) {
@@ -58,18 +82,25 @@ namespace DatingApp.API {
 
                         var error = context.Features.Get<IExceptionHandlerFeature> ();
                         if (error != null) {
-                            context.Response.AddApplicationError(error.Error.Message);
-                            await context.Response.WriteAsync(error.Error.Message);
+                            context.Response.AddApplicationError (error.Error.Message);
+                            await context.Response.WriteAsync (error.Error.Message);
                         }
                     });
                 });
             }
 
             // app.UseHttpsRedirection();
-            //seeder.SeedUsers();
+            seeder.SeedUsers();
             app.UseCors (x => x.AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
             app.UseAuthentication ();
-            app.UseMvc ();
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
+            app.UseMvc (routes => {
+                routes.MapSpaFallbackRoute (
+                    name: "spa-fallback",
+                    defaults : new { controller = "Fallback", action = "Index" }
+                );
+            });
         }
     }
 }
